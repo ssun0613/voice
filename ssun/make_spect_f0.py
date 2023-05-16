d3024990781e53b53a7c94713003a9c395ec95ed
ICS-MISUN <103228462+ICS-MISUN@users.noreply.github.com>
2023-05-16 10:12:57 +0900

    .

--- ssun/make_spect_f0.py
+++ ssun/make_spect_f0.py
@@ -42,58 +42,59 @@ targetDir = 'assets/spmel'
 dirName, subdirList, _ = next(os.walk(rootDir))
 print('Found directory: %s' % dirName)
 
-for subdir in sorted(subdirList):
-    print(subdir)
+if __name__=='__main__':
+    for subdir in sorted(subdirList):
+        print(subdir)
 
-    if not os.path.exists(os.path.join(targetDir, subdir)):
-        os.makedirs(os.path.join(targetDir, subdir))
-    if not os.path.exists(os.path.join(targetDir_f0, subdir)):
-        os.makedirs(os.path.join(targetDir_f0, subdir))
-    _, _, fileList = next(os.walk(os.path.join(dirName, subdir)))
+        if not os.path.exists(os.path.join(targetDir, subdir)):
+            os.makedirs(os.path.join(targetDir, subdir))
+        if not os.path.exists(os.path.join(targetDir_f0, subdir)):
+            os.makedirs(os.path.join(targetDir_f0, subdir))
+        _, _, fileList = next(os.walk(os.path.join(dirName, subdir)))
 
-    # if spk2gen[subdir] == 'M':
-    #     lo, hi = 50, 250
-    # elif spk2gen[subdir] == 'F':
-    #     lo, hi = 100, 600
-    # else:
-    #     raise ValueError
-    lo, hi = 50, 600
+        # if spk2gen[subdir] == 'M':
+        #     lo, hi = 50, 250
+        # elif spk2gen[subdir] == 'F':
+        #     lo, hi = 100, 600
+        # else:
+        #     raise ValueError
+        lo, hi = 50, 600
 
-    prng = RandomState(int(subdir[1:]))
-    for fileName in sorted(fileList):
-        x, fs = sf.read(os.path.join(dirName, subdir, fileName))
-        assert fs == 16000
-        if x.shape[0] % 256 == 0:
-            x = np.concatenate((x, np.array([1e-06])), axis=0)
-        # Apply a digital filter forward and backward to a signal.
-        y = signal.filtfilt(b, a, x)
-        # Add a noise signal, which is (prng.rand(y.shape[0]) - 0.5) * 1e-06.
-        wav = y * 0.96 + (prng.rand(y.shape[0]) - 0.5) * 1e-06
+        prng = RandomState(int(subdir[1:]))
+        for fileName in sorted(fileList):
+            x, fs = sf.read(os.path.join(dirName, subdir, fileName))
+            assert fs == 16000
+            if x.shape[0] % 256 == 0:
+                x = np.concatenate((x, np.array([1e-06])), axis=0)
+            # Apply a digital filter forward and backward to a signal.
+            y = signal.filtfilt(b, a, x)
+            # Add a noise signal, which is (prng.rand(y.shape[0]) - 0.5) * 1e-06.
+            wav = y * 0.96 + (prng.rand(y.shape[0]) - 0.5) * 1e-06
 
-        D = pySTFT(wav, fft_length=1024, hop_length=256).T
-        D_mel = np.dot(D, mel_basis)
-        D_db = 20 * np.log10(np.maximum (min_level, D_mel)) - 16
-        S = (D_db + 100) / 100
+            D = pySTFT(wav, fft_length=1024, hop_length=256).T
+            D_mel = np.dot(D, mel_basis)
+            D_db = 20 * np.log10(np.maximum (min_level, D_mel)) - 16
+            S = (D_db + 100) / 100
 
-        D_log = 20 * np.log10(D_mel.T)
-        mfcc = fft.dct(D_log, axis=0, norm='ortho').T
-        mfcc[:, 0:4] = 0
-        mfcc = minmax_scale(mfcc, axis=1)
+            D_log = 20 * np.log10(D_mel.T)
+            mfcc = fft.dct(D_log, axis=0, norm='ortho').T
+            mfcc[:, 0:4] = 0
+            mfcc = minmax_scale(mfcc, axis=1)
 
-        # extract f0
-        # 2**15 = 32768 --> available maximum value for a speech signal
-        # 256 may be a hop length. 256/fs = 256/16000 [sec]
-        f0_rapt = sptk.rapt(wav.astype(np.float32) * 32768, fs, 256, min=lo, max=hi, otype='pitch')
-        index_nonzero = (f0_rapt != -1e10)
-        mean_f0, std_f0 = np.mean(f0_rapt[index_nonzero]), np.std(f0_rapt[index_nonzero])
-        f0_norm = speaker_normalization(f0_rapt, index_nonzero, mean_f0, std_f0)
+            # extract f0
+            # 2**15 = 32768 --> available maximum value for a speech signal
+            # 256 may be a hop length. 256/fs = 256/16000 [sec]
+            f0_rapt = sptk.rapt(wav.astype(np.float32) * 32768, fs, 256, min=lo, max=hi, otype='pitch')
+            index_nonzero = (f0_rapt != -1e10)
+            mean_f0, std_f0 = np.mean(f0_rapt[index_nonzero]), np.std(f0_rapt[index_nonzero])
+            f0_norm = speaker_normalization(f0_rapt, index_nonzero, mean_f0, std_f0)
 
-        assert len(S) == len(f0_rapt)
+            assert len(S) == len(f0_rapt)
 
-        np.save(os.path.join(targetDir, subdir, fileName[:-4]),
-                S.astype(np.float32), allow_pickle=False)
-        np.save(os.path.join(targetDir_f0, subdir, fileName[:-4]),
-                f0_norm.astype(np.float32), allow_pickle=False)
+            np.save(os.path.join(targetDir, subdir, fileName[:-4]),
+                    S.astype(np.float32), allow_pickle=False)
+            np.save(os.path.join(targetDir_f0, subdir, fileName[:-4]),
+                    f0_norm.astype(np.float32), allow_pickle=False)
 
 
 #   https://github.com/r9y9/pysptk/blob/master/pysptk/sptk.py
