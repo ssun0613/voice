3309860051774167481be30e0cf7467f6979ba9b
harimYi <kinglee214@gmail.com>
2023-05-16 19:50:10 +0900

    .

--- ssun/utils.py
+++ ssun/utils.py
@@ -1,8 +1,5 @@
-import copy
-import torch
 import numpy as np
 from scipy import signal
-from librosa.filters import mel
 from scipy.signal import get_window
 
 import matplotlib
@@ -10,13 +7,11 @@ import torch
 matplotlib.use("Agg")
 import matplotlib.pylab as plt
 
-
 def butter_highpass(cutoff, fs, order=5):
     nyq = 0.5 * fs
     normal_cutoff = cutoff / nyq
     b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
     return b, a
-
 
 def pySTFT(x, fft_length=1024, hop_length=256):
     x = np.pad(x, int(fft_length // 2), mode='reflect')
@@ -24,14 +19,12 @@ def pySTFT(x, fft_length=1024, hop_length=256):
     noverlap = fft_length - hop_length
     shape = x.shape[:-1] + ((x.shape[-1] - noverlap) // hop_length, fft_length)
     strides = x.strides[:-1] + (hop_length * x.strides[-1], x.strides[-1])
-    result = np.lib.stride_tricks.as_strided(x, shape=shape,
-                                             strides=strides)
+    result = np.lib.stride_tricks.as_strided(x, shape=shape, strides=strides)
 
     fft_window = get_window('hann', fft_length, fftbins=True)
     result = np.fft.rfft(fft_window * result, n=fft_length).T
 
     return np.abs(result)
-
 
 def speaker_normalization(f0, index_nonzero, mean_f0, std_f0):
     # f0 is logf0
@@ -41,7 +34,6 @@ def speaker_normalization(f0, index_nonzero, mean_f0, std_f0):
     f0[index_nonzero] = np.clip(f0[index_nonzero], -1, 1)
     f0[index_nonzero] = (f0[index_nonzero] + 1) / 2.0
     return f0
-
 
 def quantize_f0_numpy(x, num_bins=256):
     # x is logf0
@@ -57,7 +49,6 @@ def quantize_f0_numpy(x, num_bins=256):
     enc[np.arange(len(x)), x.astype(np.int32)] = 1.0
     return enc, x.astype(np.int64)
 
-
 def quantize_f0_torch(x, num_bins=256):
     # x is logf0
     B = x.size(0)
@@ -72,18 +63,15 @@ def quantize_f0_torch(x, num_bins=256):
     enc[torch.arange(x.size(0)), x.long()] = 1
     return enc.view(B, -1, num_bins + 1), x.view(B, -1).long()
 
-
 def get_mask_from_lengths(lengths, max_len):
     ids = torch.arange(0, max_len, device=lengths.device)
     mask = (ids >= lengths.unsqueeze(1)).bool()
     return mask
 
-
 def pad_seq_to_2(x, len_out=128):
     len_pad = (len_out - x.shape[1])
     assert len_pad >= 0
     return np.pad(x, ((0, 0), (0, len_pad), (0, 0)), 'constant'), len_pad
-
 
 # ================
 
