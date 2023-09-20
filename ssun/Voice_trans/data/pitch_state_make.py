fe1487b43f4a298588410a03460918206bbd8256
harimYi <kinglee214@gmail.com>
2023-09-20 10:28:12 +0900

    .

--- ssun/Voice_trans/data/pitch_state_make.py
+++ ssun/Voice_trans/data/pitch_state_make.py
@@ -8,6 +8,7 @@ from scipy import signal
 
 from numpy.random import RandomState
 from pysptk import sptk
+import pyworld as pw
 from sklearn.preprocessing import StandardScaler
 
 def normalize(in_dir, mean, std):
@@ -24,7 +25,7 @@ def normalize(in_dir, mean, std):
     return min_value, max_value
 
 if __name__=='__main__':
-    np.load('/storage/mskim/English_voice/make_dataset/new/pitch_state.npy')
+    # np.load('/storage/mskim/English_voice/make_dataset/new/pitch_state.npy')
 
     dataset_path = sorted(glob.glob('/storage/mskim/English_voice/train/' + "*/*.wav"))
     path = '/storage/mskim/English_voice/make_dataset/'
@@ -34,7 +35,7 @@ if __name__=='__main__':
     lo, hi = 50, 600
     b, a = signal.butter(N=5, Wn=30, fs=fs,btype='high')
 
-    os.makedirs((os.path.join(path, "new/make_pitch(only_pitch)")), exist_ok=True)
+    os.makedirs((os.path.join(path, "new/make_pitch(only_pitch_2)")), exist_ok=True)
     pitch_scaler = StandardScaler()
 
     for index in range(0, len(dataset_path)):
@@ -49,16 +50,20 @@ if __name__=='__main__':
 
         seed = RandomState(int(data.split('/')[-1].split('_')[-1][:-4]))
         wav = data_filt * 0.96 + (seed.rand(data_filt.shape[0]) - 0.5) * 1e-06
-        pitch = sptk.rapt(wav.astype(np.float32) * 32768, fs, hopsize=256, min=lo, max=hi, otype='pitch')
+        _f0, t = pw.dio(wav, fs)
+        pitch = pw.stonemask(wav, _f0, t, fs)
+        np.save(path + 'new/make_pitch(only_pitch_2)/{}_{}'.format(data.split('/')[-2], data.split('/')[-1][:-4]),pitch.astype(np.float32), allow_pickle=False)
+        print("save {}_{}".format(data.split('/')[-2], data.split('/')[-1][:-4]))
 
         if len(pitch>0):
             pitch_scaler.partial_fit(pitch.reshape((-1,1)))
-        np.save(path + 'new/make_pitch(only_pitch)/{}_{}'.format(data.split('/')[-2], data.split('/')[-1][:-4]), pitch.astype(np.float32), allow_pickle=False)
+
+    print("\nfinish")
 
     pitch_mean = pitch_scaler.mean_[0]
     pitch_std = pitch_scaler.scale_[0]
-    pitch_min, pitch_max = normalize(os.path.join(path, "new/make_pitch(only_pitch)"), pitch_mean, pitch_std)
+    pitch_min, pitch_max = normalize(os.path.join(path, "new/make_pitch(only_pitch_2)"), pitch_mean, pitch_std)
 
     stats = {"pitch_min": float(pitch_min), "pitch_max": float(pitch_max), "pitch_mean": float(pitch_mean), "pitch_std": float(pitch_std)}
-    np.save(path + 'new/pitch_state.npy',stats)
+    np.save(path + 'new/pitch_state_2.npy',stats)
 
